33257f0423d178af2743d2ea30c6a4ed0315079a
inno1OO <louicent19@gmail.com>
2018-05-26 16:03:51 -0400

    ok

--- index.php
+++ index.php
@@ -231,22 +231,9 @@
    
 </div> </div> </div> </div>
 <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="plugins/iCheck/icheck.min.js"></script>
-<script src="bower_components/PACE/pace.min.js"></script>
-<!-- PACE -->
-<script src="bower_components/PACE/pace.min.js"></script>
-<!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<?php  
+include("includes/scripts.php");
+?>
 
 <script>
   $(function () {
